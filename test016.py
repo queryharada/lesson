c463d35c5a408b9680a0312862ecc802f42266a9
harada <queryharada@gmail.com>
2019-08-16 11:24:58 +0900

    update

--- test016.py
+++ test016.py
@@ -21,12 +21,14 @@ def index():
 def router():
     messageId = '000'
 
+    # 処理が選択されていません'
     if not 'procSelect' in request.form:
         rowList = selectData()
-        messageId = '001'  # '処理が選択されていません'
+        messageId = '001'
         companyId = ''
         return makeHtml(rowList, companyId, messageId)
 
+    # 会社一覧選択
     if request.form['procSelect'] == 'select':
         if not 'companyId' in request.form:
             messageId = '002'  # ''会社一覧が選択されていません'
@@ -37,16 +39,24 @@ def router():
         rowList = selectData()
         return makeHtml(rowList, companyId, messageId)
 
+    # 新規
     if request.form['procSelect'] == 'insert':
         companyId, messageId = insertData(request.form)
         rowList = selectData()
         return makeHtml(rowList, companyId, messageId)
 
+    # 更新
     if request.form['procSelect'] == 'update':
-        companyId, messageId = updateData(request.form)
+        if not 'companyId' in request.form:
+            messageId = '002'  # ''会社一覧が選択されていません'
+            companyId = ''
+        else:
+            companyId, messageId = updateData(request.form)
+
         rowList = selectData()
         return makeHtml(rowList, companyId, messageId)
 
+    # 削除
     if request.form['procSelect'] == 'delete':
         if not 'companyId' in request.form:
             messageId = '002'  # ''会社一覧が選択されていません'
