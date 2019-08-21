bee03222510093e6588d106c03d2e747cfc8f182
harada <queryharada@gmail.com>
2019-08-21 16:28:51 +0900

    update

--- test010.py
+++ test010.py
@@ -1,7 +1,14 @@
 # -*- coding:utf-8 -*-
 
-for i in range(1,10):
+def rj(num, digit):
+    numStr = str(num)
+    padingDigit = digit - len(numStr)
+    return ' ' * padingDigit + numStr
+
+
+for i in range(1, 10):
     line = ''
-    for j in range(1,10):
-        line += str(i*j).rjust(4)
+    for j in range(1, 10):
+        # line += str(i*j).rjust(4)
+        line += rj(i * j, 4)
     print(line)
