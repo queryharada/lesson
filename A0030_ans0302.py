980a085079f9fc7ef9fa3b09ce5a052c112d664e
harada <queryharada@gmail.com>
2019-08-27 19:55:16 +0900

    update

--- A0030_ans0302.py
+++ A0030_ans0302.py
@@ -4,6 +4,7 @@ BASESTRING = '0123456789ABCDEF'
 ENTERCOUNT = 4
 
 
+# HEX文字列を数値へ変換
 def hexToInt(hexStr):
     i = len(hexStr)
     value = 0
@@ -15,6 +16,7 @@ def hexToInt(hexStr):
     return value
 
 
+# 数値をHEX文字列へ変換
 def intToHex(intVal):
     if intVal == 0:
         return '00'
@@ -24,13 +26,16 @@ def intToHex(intVal):
         modV = intVal % len(BASESTRING)
         hexStr += BASESTRING[modV]
         intVal = int(intVal / len(BASESTRING))
+    # 文字列をリバース
     return hexStr[::-1]
 
-
+# HEX文字列入力
 numList = []
 while len(numList) < ENTERCOUNT:
     inputStr = input("Please Enter Number(HEX): ")
     inputStr = inputStr.upper()
+
+    # HEX文字列チェック
     isNumChk = True
     for oneChar in inputStr:
         if not oneChar in BASESTRING:
@@ -42,7 +47,7 @@ while len(numList) < ENTERCOUNT:
     else:
         print('Enter Number is not HEX format')
 
-print(numList)
+# 最小最大を求める
 minValue = -1
 maxValue = 0
 totValue = 0
@@ -53,9 +58,11 @@ for v in numList:
         maxValue = v
     totValue += v
 
-print('min value :', intToHex(minValue))
-print('max value :', intToHex(maxValue))
-print('total value :', intToHex(totValue))
+# 最小最大平均を出力
+print(numList)
+print('min value     :', intToHex(minValue))
+print('max value     :', intToHex(maxValue))
+print('total value   :', intToHex(totValue))
 print('average value :', intToHex(int(totValue / ENTERCOUNT)))
 
 if __name__ == "__main__":
