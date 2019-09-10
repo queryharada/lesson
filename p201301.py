a8c9fdf91454ffefeb198bdd21a8df746962f7d9
harada <queryharada@gmail.com>
2019-09-10 17:11:08 +0900

    update

--- p201301.py
+++ p201301.py
@@ -18,45 +18,36 @@ def getcombs(a, combo=None):
 
 
 def getMulMax(num):
-    numDiv1 = 0
     numDiv2 = 0
     numDiv3 = int(num / 3)
-    if numDiv3 == 1:
-        numDiv3 = 0
-        numMod3 = num
-    else:
-        numMod3 = num % 3
+    numMod3 = num % 3
     if numMod3 != 0:
-        numDiv2 = int(numMod3 / 2)
-        numMod2 = numMod3 % 2
-        if numMod2 == 1:
-            numDiv1 = 1
-    return numDiv1, numDiv2, numDiv3
+        if ((numMod3 + 3) % 2) == 0:
+            numDiv2 = int(numMod3 + 3 / 2)
+            numDiv3 -= 1
+        else:
+            numDiv2 = int(numMod3 / 2)
+
+    return numDiv2, numDiv3
 
 
 if __name__ == "__main__":
-    num = 13
-    combos = getcombs(num)
-    maxMul = 0
-    maxCombo = None
-    for combo in combos:
-        print(combo)
-        tempMul = 1
-        for v in combo:
-            tempMul = tempMul * v
-        if maxMul < tempMul:
-            maxMul = tempMul
-            maxCombo = combo
+    for num in range(2,20):
+        combos = getcombs(num)
+        maxMul = 0
+        maxCombo = None
+        for combo in combos:
+            # print(combo)
+            tempMul = 1
+            for v in combo:
+                tempMul = tempMul * v
+            if maxMul < tempMul:
+                maxMul = tempMul
+                maxCombo = combo
+        print('Num:',num)
+        print('MAX Comb:', maxCombo, ' = ', maxMul)
+        numDiv2, numDiv3 = getMulMax(num)
+        print('MAX Comb:', numDiv2, numDiv3, ' = ', (2 ** numDiv2) * (3 ** numDiv3))
 
-    print('MAX Comb:', maxCombo, ' = ', maxMul)
-
-    numDiv1, numDiv2, numDiv3 = getMulMax(num)
-    numTot = 0
-    if numDiv1 != 0:
-        numTot += 1 ** numDiv1
-    if numDiv2 != 0:
-        numTot += 2 ** numDiv2
-    if numDiv3 != 0:
-        numTot += 3 ** numDiv3
-
-    print('MAX Comb:', numDiv1, numDiv2, numDiv3, ' = ', numTot)
+    numDiv2, numDiv3 = getMulMax(2001)
+    print('MAX Comb:', numDiv2, numDiv3, ' = ', (2 ** numDiv2) * (3 ** numDiv3))
