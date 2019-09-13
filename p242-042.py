e5c7b03c9e2b62741963420db4126d8ef0dcbf35
harada <queryharada@gmail.com>
2019-09-13 11:03:10 +0900

    update

--- p242-042.py
+++ p242-042.py
@@ -1,16 +1,15 @@
 def salByDay1(sal):
-    for i in range(1, 31):
-        print(i, ':', sal)
+    for days in range(1, 31):
+        print(str(days).rjust(2), ':', "{:,}".format(sal).rjust(11))
         sal = sal * 2
 
 
 def salByDay2(days, sal):
-    if days > 30:
-        return
+    if days < 31:
+        print(str(days).rjust(2), ':', "{:,}".format(sal).rjust(11))
+        return salByDay2(days + 1, sal * 2)
 
-    print(days, ':', sal)
-    salByDay2(days + 1, sal * 2)
-
+    return
 
 if __name__ == "__main__":
     salByDay1(1)
